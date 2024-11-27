9867f640523eea47c94f920c9479879cdf8d2304
masum-abrar <masum.abrar02@gmail.com>
2024-11-27 21:11:25 +0600

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Skips ESLint checks during builds
+  },
 };
 
 export default nextConfig;
